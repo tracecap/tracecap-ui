45d77610558ce2b99b978d3f1db6fe9ce9c519d4
Theo Julienne <theojulienne@github.com>
2021-05-05 09:33:36 +1000

    Fix missing import.

--- tracecap_viewer/tracecap_viewer/settings.py
+++ tracecap_viewer/tracecap_viewer/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os, sys
 from django.core.management.utils import get_random_secret_key
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
