83b6dca5410b6918fb5bd775c3e9b04842c1b819
Theo Julienne <theojulienne@github.com>
2021-05-05 08:13:45 +1000

    collectstatic at the end of building dist.

--- dist-update.sh
+++ dist-update.sh
@@ -2,9 +2,16 @@
 
 (
     set -ex
-    
+
     cd perf-inspect
     yarn tsc
     yarn rollup -c
     sass sass/main.scss dist/tracecap-viewer.css
+)
+
+(
+    set -ex
+
+    cd tracecap_viewer
+    python manage.py collectstatic
 )
